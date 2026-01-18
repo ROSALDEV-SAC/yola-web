ef2454b6b1bd092757b6a176bc75f77b281c5f7b
RYZEN3 <rosaldev.public@gmail.com>
2026-01-17 20:58:53 -0500

    Lanzamiento web YOLA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',  // <--- ESTO ES LA CLAVE MÁGICA
+    images: {
+        unoptimized: true, // Necesario para GitHub Pages
+    },
+    // Si vas a usar un subdominio como /yola, descomenta esto:
+    // basePath: '/yola',
+};
 
 export default nextConfig;
